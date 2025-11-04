faab8b3c143ea14e4dc0c8da0a9fb1237f175440
grant777 <nnayz21062gmail.com>
2025-11-04 08:04:26 +0800

    created streamwriter

--- FileProcessing_pract/Form1.vb
+++ FileProcessing_pract/Form1.vb
@@ -1,3 +1,17 @@
 ﻿Public Class Form1
+    Private Sub ButtonWrite_Click(sender As Object, e As EventArgs) Handles ButtonWrite.Click
+        Try
+            Dim filepath As String = "sample text" 'file location'
 
+            Using writer As New StreamWriter(filepath)
+                writer.WriteLine("HEllo, this is a test file!")
+                writer.WriteLine("Second line of the text")
+            End Using
+
+            MessageBox.Show("Data Writter succesfully!")
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Error", )
+
+    End Sub
 End Class
